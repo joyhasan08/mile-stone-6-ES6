2f821d55f1d8c21ba03d104e80f69ef71055e5be
Zarjij Hasan joy <rubayedhasan235@gmail.com>
2023-08-25 01:59:52 +0600

    Arrow function

--- Arrow fuction/arrowFuntion.js
+++ Arrow fuction/arrowFuntion.js
@@ -1,1 +1,15 @@
 console.log("hello");
+let aPerson = {
+    names: `joy`,
+    age: 25,
+    address: `Dhaka`,
+    phone: 01788450096,
+    email: `joyhasan32@gmail.com`  
+}
+//arrow function
+const add = (a,b) => a+b;
+console.log(add(5,4));
+const devison = (num1,num2) => num1/num2;
+console.log(devison(10,2));
+const isAge = (persion)=> persion.age
+console.log(isAge(aPerson));
